ff242aafbcb0a7e75b83a12e637c8885d2b17be5
Some_Dumb_Dude <154841872+Tuxedp@users.noreply.github.com>
2025-12-27 14:55:35 -0500

    UI + Basic Navigation Code

---
+++ Ohmwards/src/main/java/com/mycompany/ohmwards/Ohmwards.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ohmwards;
+
+/**
+ *
+ * @author Cameron
+ */
+public class Ohmwards {
+    public static MainMenu main;
+    
+    public static void main(String[] args) {
+        main = new MainMenu();
+        main.setVisible(true);
+    }
+}
