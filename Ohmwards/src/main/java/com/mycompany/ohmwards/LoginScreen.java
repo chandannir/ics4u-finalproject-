4fd0b154fbe97579584e7a5a2315045e466b0882
Some_Dumb_Dude <154841872+Tuxedp@users.noreply.github.com>
2026-01-20 08:13:01 -0500

    Update LoginScreen.java

--- Ohmwards/src/main/java/com/mycompany/ohmwards/LoginScreen.java
+++ Ohmwards/src/main/java/com/mycompany/ohmwards/LoginScreen.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package com.mycompany.ohmwards;
-import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import java.nio.file.*;
 import java.io.*;
